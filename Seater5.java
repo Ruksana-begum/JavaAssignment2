1edf1736e4c4e4c9131f0cc03a56afd4be0f87b8
Ruksana-begum <127771632+Ruksana-begum@users.noreply.github.com>
2023-07-09 11:23:25 +0530

    Create Seater5.java

---
+++ Seater5.java
@@ -0,0 +1,12 @@
+package Rate;
+
+public class Seater5 {
+	String name3="C";
+	    double rate3=4.8;
+	    int distfrom3=200;
+	    public void print() {
+	    	if(rate3>=4) {
+	          System.out.println("driver "+name3+" is get you to the destination");
+	    	}
+	    }
+	}
